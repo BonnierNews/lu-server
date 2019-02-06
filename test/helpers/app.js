c3db89a2982110a46baf08bf5780de8bc1c4ff9d
Jens Carlén <jens.carlen@bonniernews.se>
2019-02-06 07:46:26 +0100

    Moved code around

--- test/helpers/app.js
+++ test/helpers/app.js
@@ -4,20 +4,20 @@ const buildApp = require("../../").buildApp;
 
 const requests = [];
 const router = express.Router(); // eslint-disable-line new-cap
+router.use((req, res, next) => {
+  requests.push({path: req.path, req, res});
+  next();
+});
 router.get("/some-path", (req, res) => {
-  requests.push({req, res});
   res.status(200).send("Yes");
 });
 router.get("/error", (req, res, next) => {
-  requests.push({req, res});
   next(new Error("Foo"));
 });
 router.get("/timeouterror", (req, res, next) => {
-  requests.push({req, res});
   setTimeout(() => next(new Error("we did timeout")), 10);
 });
-router.get("/bork", (req, res) => {
-  requests.push({req, res});
+router.get("/bork", () => {
   throw new Error("Bork!");
 });
 
