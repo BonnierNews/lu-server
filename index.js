a5542c92f5b49c72265796b07a3977616af1bd85
MattiasOlla <mattias.olla@gmail.com>
2024-08-07 14:53:47 +0200

    WIP: longer keepalive and headers timeouts (#75)
    * Try increasing keepalive and headers timeouts to prevent socket hang ups
    
    * Bump version

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import config from "exp-config";
-import { logger } from "lu-logger";
 import * as fs from "fs";
+import { logger } from "lu-logger";
 import * as path from "path";
 
-import shutdownHandler from "./lib/shutdownHandler.js";
+import buildApp from "./lib/build-app.js";
 import { registerServerTerminator } from "./lib/handle-sigterm.js";
 import { render404, render409, renderError } from "./lib/render-error.js";
+import shutdownHandler from "./lib/shutdownHandler.js";
 import validator from "./lib/validator.js";
-import buildApp from "./lib/build-app.js";
 
 export function init(routes) {
   const app = buildApp(routes);
@@ -17,6 +17,9 @@ export function init(routes) {
 
     const port = Number(process.env.PORT) || config.port || 3000;
     const server = app.listen(port, () => {
+      server.keepAliveTimeout = 120 * 1000;
+      server.headersTimeout = 130 * 1000;
+
       logger.info(`${packageInfo.name} listening on port ${server.address().port}`);
     });
     logger.info("initiating sigterm handler with shutdown handler");
